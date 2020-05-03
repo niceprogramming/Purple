c16c37bf3d415a4cffd0399648a511e57070bea5
niceprogramming <niceprogramming@github.com>
2020-05-03 14:45:04 -0500

    Add project files.

---
+++ Purple.Database/PurpleContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Purple.Database
+{
+    public class PurpleContext : DbContext
+    {
+        public DbSet<Ban> Bans { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+
+    public class Ban
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
